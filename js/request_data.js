e7f053769aabc11fd5b16ec59ba2d25fbcb6bf9a
Samuel Lager <sambenuts.contact@gmail.com>
2019-01-10 22:10:09 +0100

    Fix Emna merge

--- js/request_data.js
+++ js/request_data.js
@@ -1,5 +1,5 @@
-let requestURL = './json/data.json';
-let request = new XMLHttpRequest(); //
-request.open('GET', requestURL); //
-request.responseType = 'json';
-request.send(); //
+let requestURL = './json/data.json'; //url vers le fichier JSON qui contient les données du site
+let request = new XMLHttpRequest(); //on crée un objet qui va interagir avec le serveur
+request.open('GET', requestURL); //initialise la requête vers l'url du fichier JSON
+request.responseType = 'json'; //on précise que l'on veut un objet JSON
+request.send(); //envoie la requête et attend une réponse
